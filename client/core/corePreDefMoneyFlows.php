8ec038d892596d59e10e92a076e4f24fb0bac376
olivleh1 <olivleh1>
2005-03-05 00:11:50 +0000

    beautiify

--- client/core/corePreDefMoneyFlows.php
+++ client/core/corePreDefMoneyFlows.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+	$Id: corePreDefMoneyFlows.php,v 1.2 2005/03/05 00:11:50 olivleh1 Exp $
+*/
+
 require_once 'core/core.php';
 require_once 'core/coreContractPartners.php';
 
@@ -20,12 +24,12 @@ class corePreDefMoneyFlows extends core {
 
 	function get_all_index_letters() {
 		$temp=$this->select_cols( "SELECT DISTINCT contractpartnerid FROM predefmoneyflows" );
-		return $this->coreContractPartners->get_ids_index_letters($temp);
+		return $this->coreContractPartners->get_ids_index_letters( $temp );
 	}
 
 	function get_all_matched_data( $letter ) {
 		$ids=$this->coreContractPartners->get_ids_matched_data( $letter );
-		$idstring=implode($ids,",");
+		$idstring=implode( $ids, "," );
 		return $this->select_rows( "SELECT * FROM predefmoneyflows  WHERE contractpartnerid IN ($idstring) ORDER BY comment" );
 	}
 
@@ -35,11 +39,11 @@ class corePreDefMoneyFlows extends core {
 	}
 
 
-	function update_predefmoneyflow( $id,$amount,$capitalsourceid,$contractpartnerid,$comment ) {
-		return $this->insert_row( "UPDATE predefmoneyflows set amount='$amount',capitalsourceid='$capitalsourceid',contractpartnerid='$contractpartnerid',comment='$comment' WHERE id=$id;" );
+	function update_predefmoneyflow( $id, $amount, $capitalsourceid, $contractpartnerid, $comment ) {
+		return $this->insert_row( "UPDATE predefmoneyflows set amount='$amount',capitalsourceid='$capitalsourceid',contractpartnerid='$contractpartnerid',comment='$comment' WHERE id=$id" );
 	}
 
-	function add_predefmoneyflow( $amount,$capitalsourceid,$contractpartnerid,$comment ) {
-		return $this->insert_row( "INSERT INTO predefmoneyflows (amount,capitalsourceid,contractpartnerid,comment) VALUES ('$amount','$capitalsourceid','$contractpartnerid','$comment');" );
+	function add_predefmoneyflow( $amount, $capitalsourceid, $contractpartnerid, $comment ) {
+		return $this->insert_row( "INSERT INTO predefmoneyflows (amount,capitalsourceid,contractpartnerid,comment) VALUES ('$amount','$capitalsourceid','$contractpartnerid','$comment')" );
 	}
 }
