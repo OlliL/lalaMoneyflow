c68a6a88ad65a76ef3dedb28b483585d360a1ba7
olivleh1 <olivleh1>
2007-07-28 19:49:25 +0000

    add comments + copyright notes

--- client/include.php
+++ client/include.php
@@ -1,9 +1,54 @@
 <?php
+#-
+# Copyright (c) 2005-2007 Oliver Lehmann <oliver@FreeBSD.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#	notice, this list of conditions and the following disclaimer
+# 2. Redistributions in binary form must reproduce the above copyright
+#	notice, this list of conditions and the following disclaimer in the
+#	documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# $Id: include.php,v 1.7 2007/07/28 19:49:25 olivleh1 Exp $
+#
 
-$confTimer = 2;
-$money_debug = false;
-$dsn = "mysql://moneyflow:moneyflow@db/moneyflow";
+##########
+# user defined variables
+##########
+
+# MySQL config - syntax: mysql://user/password@server/database
+$dsn = "mysql://moneyflow:moneyflow@db/moneyflow"; 
+
+# jpgraph is used for plotting trends
 define( 'ENABLE_JPGRAPH', true );
+
+
+##########
+# more or less system defined stuff following
+##########
+
+# style how the timer information is printed out in debug mode (int 0-2)
+$confTimer   = 2;
+
+# debug mode (boolean)
+$money_debug = false;
+
+# default year for "valid til" columns when creating a new dataset
 define( 'MAX_YEAR', '2999-12-31' );
 
 ?>
