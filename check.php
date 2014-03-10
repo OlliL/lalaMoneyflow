86a7578f62005510f56439d2f290ae8defc19dd0
olivleh1 <olivleh1>
2014-03-10 20:58:11 +0000

    more work on setup checking

--- check.php
+++ check.php
@@ -21,47 +21,66 @@ td {
 
 </head>
 <body>
-	<center><h1>Checking your Setup...</h1></center><br><br>
+	<center>
+		<h1>Checking your Setup...</h1>
+	</center>
+	<br>
+	<br>
 	<table width="600" align="center">
 <?php
+include "base/Configuration.php";
+use base\Configuration;
+
+function echo_row($leftText, $rightText, $ok) {
+	if ($leftText) {
+		echo '<tr><td class="contrastbgcolor">' . $leftText . '</td><td class="contrastbgcolor" align="center">';
+	} else {
+		echo '<tr><td>&nbsp;</td><td align="center" class="contrastbgcolor">';
+	}
+	if (! $ok) {
+		echo '<font color="red">' . $rightText . '</font>';
+	} else {
+		echo '<font color="green">' . $rightText . '</font>';
+	}
+	echo '</td></tr>';
+}
 
 function check_extension($extension, $text = null) {
 	if ($text === null)
 		$text = $extension;
-	echo '<tr><td class="contrastbgcolor">checking for extension ' . $text . '</td><td class="contrastbgcolor" align="center">';
 	if (! extension_loaded( $extension )) {
-		echo '<font color="red">not installed</font>';
+		echo_row( 'checking for extension ' . $text, 'not installed', false );
 		return 1;
 	} else {
-		echo '<font color="green">installed</font>';
+		echo_row( 'checking for extension ' . $text, 'installed', true );
 		return 0;
 	}
 }
 
 function check_class($class, $text) {
-	echo '<tr><td class="contrastbgcolor">checking for ' . $text . '</td><td class="contrastbgcolor" align="center">';
 	if (! class_exists( $class )) {
-		echo '<font color="red">not installed</font>';
+		echo_row( 'checking for ' . $text, 'not installed', false );
 		return 1;
 	} else {
-		echo '<font color="green">installed</font>';
+		echo_row( 'checking for ' . $text, 'installed', true );
 		return 0;
 	}
 }
 
 function print_result($errors, $allowedErrorCount) {
-	echo '<tr><td>&nbsp;</td><td align="center" class="contrastbgcolor">';
 	if ($errors <= $allowedErrorCount)
-		echo '<font color="green">OK</font>';
+		echo echo_row( null, 'OK', true );
 	else
-		echo '<font color="red">not OK</font>';
-	echo '</td><tr>';
+		echo echo_row( null, 'not OK', false );
 }
 
 function print_headline($headline) {
 	echo '<tr><td colspan="2" align="center">' . $headline . '</td><tr>';
 }
 
+/*
+ * *************************************************************************************
+ */
 
 print_headline( 'Checking PHP, PEAR, PECL extensions (all needed)' );
 $errors = 0;
@@ -78,8 +97,6 @@ $errors += check_extension( 'simplexml' );
 $errors += check_extension( 'SPL_Types' );
 print_result( $errors, 0 );
 
-
-
 print_headline( 'Checking existing Cache Frameworks (minimum 1 needed)' );
 $errors = 0;
 $errors += check_extension( 'yac', '<a href="https://github.com/laruence/yac">yac</a>' );
@@ -87,7 +104,9 @@ $errors += check_extension( 'memcached' );
 $errors += check_extension( 'redis' );
 print_result( $errors, 2 );
 
-
+/*
+ * *************************************************************************************
+ */
 
 print_headline( 'Checking additional requirements' );
 $errors = 0;
@@ -97,15 +116,135 @@ $errors += check_class( 'Smarty', '<a href="http://www.smarty.net/">Smarty Templ
 $errors += check_class( 'Graph', '<a href="http://jpgraph.net/">JpGraph</a>' );
 print_result( $errors, 0 );
 
-print_headline( 'Checking client/include.php' );
-include 'client/include.php';
-echo ROOTDIR.'<br>';
-define('ROOTDIR','foo');
-echo ROOTDIR.'<br>';
-print_headline( 'Checking server/include.php' );
-include 'server/include.php';
-echo server\ROOTDIR;
+/*
+ * *************************************************************************************
+ */
+
+print_headline( 'Checking client/client.properties' );
+$errors = 0;
+Configuration::getInstance()->readConfig( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'client/client.properties' );
+
+$context = stream_context_create( array (
+		'http' => array (
+				'header' => 'Connection: close\r\n'
+		)
+) );
+$url = Configuration::getInstance()->getProperty( 'serverurl' ) . 'admin/heartbeat';
+$result = @file_get_contents( $url, false, $context );
+if ($result != 'OK ') {
+	echo_row( 'checking Property "serverurl"', 'not OK', false );
+	$error ++;
+} else {
+	echo_row( 'checking Property "serverurl"', 'OK', true );
+}
+print_result( $errors, 0 );
+
+/*
+ * *************************************************************************************
+ */
+
+print_headline( 'Checking server/server.properties' );
+$errors = 0;
+Configuration::getInstance()->readConfig( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'server/server.properties' );
+
+$driver = Configuration::getInstance()->getProperty( 'driver', 'DATABASE' );
+if ($driver != 'mysql') {
+	echo_row( 'checking Property "DATABASE:driver"', 'not OK', false );
+	$error ++;
+} else {
+	echo_row( 'checking Property "DATABASE:driver"', 'OK', true );
+}
+
+$host = Configuration::getInstance()->getProperty( 'host', 'DATABASE' );
+$user = Configuration::getInstance()->getProperty( 'user', 'DATABASE' );
+$password = Configuration::getInstance()->getProperty( 'password', 'DATABASE' );
+$link = @mysql_connect( $host, $user, $password );
+if (! $link) {
+	echo_row( 'checking Property "DATABASE:host", "DATABASE:user", "DATABASE:password"', 'not OK', false );
+	$error ++;
+} else {
+	echo_row( 'checking Property "DATABASE:host", "DATABASE:user", "DATABASE:password"', 'OK', true );
+}
+
+$dbname = Configuration::getInstance()->getProperty( 'dbname', 'DATABASE' );
+$db_selected = @mysql_select_db( $dbname, $link );
+if (! $db_selected) {
+	echo_row( 'checking Property "DATABASE:dbname"', 'not OK', false );
+	$error ++;
+} else {
+	echo_row( 'checking Property "DATABASE:dbname"', 'OK', true );
+}
+
+$cache = Configuration::getInstance()->getProperty( 'driver', 'CACHE' );
+if ($cache != 'REDIS' && $cache != 'MEMCACHED' && $cache != 'YAC' && $cache != 'NOOP') {
+	echo_row( 'checking Property "CACHE:driver"', 'not OK', false );
+	$error ++;
+} else {
+	echo_row( 'checking Property "CACHE:driver"', 'OK', true );
+}
+
+$cacheServer = Configuration::getInstance()->getProperty( 'server', 'CACHE' );
+$cachePort = ( int ) Configuration::getInstance()->getProperty( 'port', 'CACHE' );
+$result = null;
+switch ($cache) {
+	case 'REDIS' :
+		$cacheDelegate = new \Redis();
+		$result = $cacheDelegate->connect( $cacheServer, $cachePort );
+		break;
+	case 'MEMCACHED' :
+		$cacheDelegate = new \Memcached();
+		$result = $cacheDelegate->addServer( $cacheServer, $cachePort );
+		$version = $cacheDelegate->getStats();
+		$pid = array_values( $version )[0]['pid'];
+		$result = $pid > 0 ? true : false;
+		break;
+}
+if ($result !== null) {
+	if (! $result) {
+		echo_row( 'checking Property "CACHE:server", "CACHE:port"', 'not OK', false );
+		$error ++;
+	} else {
+		echo_row( 'checking Property "CACHE:server", "CACHE:port"', 'OK', true );
+	}
+}
+$serializer = Configuration::getInstance()->getProperty( 'driver', 'SERIALIZER' );
+if ($serializer != 'IGBINARY' && $serializer != 'PHP') {
+	echo_row( 'checking Property "SERIALIZER:driver"', 'not OK', false );
+	$error ++;
+} else {
+	echo_row( 'checking Property "SERIALIZER:driver"', 'OK', true );
+}
+
+print_result( $errors, 0 );
+
+/*
+ * *************************************************************************************
+ */
+
 print_headline( 'Checking MySQL DB' );
+$errors = 0;
+
+$name_found = false;
+if ($link) {
+	$name = null;
+	$sql = ('SELECT name FROM access WHERE id = 0');
+	$result = mysql_query( $sql, $link );
+	if ($result) {
+		$row = mysql_fetch_row( $result );
+		if (is_array( $row ) && count( $row ) === 1) {
+			$name = $row [0];
+		}
+	}
+	if ($name === 'root')
+		$nameFound = true;
+}
+if (!$nameFound) {
+	echo_row( 'checking MySQL Database', 'not OK', false );
+	$error ++;
+} else {
+	echo_row( 'checking MySQL Database', 'OK', true );
+}
+
 // check that property files can not be accessed! (.htaccess in place)
 // implement a heartbeat w/o auth in the rest server to check Slim
 ?>
