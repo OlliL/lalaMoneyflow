9a235d2c1ab743cd30c3f06bcc808924b915f120
olivleh1 <olivleh1>
2014-03-11 18:34:43 +0000

    more work on check.php

--- check.php
+++ check.php
@@ -37,19 +37,24 @@ function echo_row($leftText, $rightText, $ok) {
 	} else {
 		echo '<tr><td>&nbsp;</td><td align="center" class="contrastbgcolor">';
 	}
-	if (! $ok) {
+	if ($ok === false) {
 		echo '<font color="red">' . $rightText . '</font>';
+	} else if ($ok === null) {
+		echo '<font color="orange">' . $rightText . '</font>';
 	} else {
 		echo '<font color="green">' . $rightText . '</font>';
 	}
 	echo '</td></tr>';
 }
 
-function check_extension($extension, $text = null) {
+function check_extension($extension, $text = null, $optional = false) {
 	if ($text === null)
 		$text = $extension;
 	if (! extension_loaded( $extension )) {
-		echo_row( 'checking for extension ' . $text, 'not installed', false );
+		if ($optional)
+			echo_row( 'checking for extension ' . $text, 'not installed', null );
+		else
+			echo_row( 'checking for extension ' . $text, 'not installed', false );
 		return 1;
 	} else {
 		echo_row( 'checking for extension ' . $text, 'installed', true );
@@ -99,9 +104,24 @@ print_result( $errors, 0 );
 
 print_headline( 'Checking existing Cache Frameworks (minimum 1 needed)' );
 $errors = 0;
-$errors += check_extension( 'yac', '<a href="https://github.com/laruence/yac">yac</a>' );
-$errors += check_extension( 'memcached' );
-$errors += check_extension( 'redis' );
+$olderrors = $errors;
+$errors += check_extension( 'yac', '<a href="https://github.com/laruence/yac">yac</a>', true );
+if ($errors > $olderrors)
+	$yac_extension = false;
+else
+	$yac_extension = true;
+$olderrors = $errors;
+$errors += check_extension( 'memcached', null, true );
+if ($errors > $olderrors)
+	$memcached_extension = false;
+else
+	$memcached_extension = true;
+$olderrors = $errors;
+$errors += check_extension( 'redis', null, true );
+if ($errors > $olderrors)
+	$redis_extension = false;
+else
+	$redis_extension = true;
 print_result( $errors, 2 );
 
 /*
@@ -129,13 +149,22 @@ $context = stream_context_create( array (
 				'header' => 'Connection: close\r\n'
 		)
 ) );
-$url = Configuration::getInstance()->getProperty( 'serverurl' ) . 'admin/heartbeat';
+$serverurl = Configuration::getInstance()->getProperty( 'serverurl' );
+$url = $serverurl;
+$http_response_header = array ();
 $result = @file_get_contents( $url, false, $context );
-if ($result != 'OK ') {
-	echo_row( 'checking Property "serverurl"', 'not OK', false );
-	$error ++;
-} else {
+$parsed_url = parse_url( $url );
+if (count( $http_response_header ) == 0) {
+	echo_row( 'checking Property "serverurl"', 'Server "' . $parsed_url ['host'] . '" <br> not reachable', false );
+	$errors ++;
+} elseif (strpos( $http_response_header [0], '404' ) > 0) {
+	echo_row( 'checking Property "serverurl"', 'Path "' . $parsed_url ['path'] . '" on <br> Server "' . $parsed_url ['host'] . '" <br> not found', false );
+	$errors ++;
+} elseif (strpos( $http_response_header [0], '200' ) > 0 && $result == 'OK ') {
 	echo_row( 'checking Property "serverurl"', 'OK', true );
+} else {
+	echo_row( 'checking Property "serverurl"', 'not OK', false );
+	$errors ++;
 }
 print_result( $errors, 0 );
 
@@ -150,7 +179,7 @@ Configuration::getInstance()->readConfig( dirname( __FILE__ ) . DIRECTORY_SEPARA
 $driver = Configuration::getInstance()->getProperty( 'driver', 'DATABASE' );
 if ($driver != 'mysql') {
 	echo_row( 'checking Property "DATABASE:driver"', 'not OK', false );
-	$error ++;
+	$errors ++;
 } else {
 	echo_row( 'checking Property "DATABASE:driver"', 'OK', true );
 }
@@ -161,7 +190,7 @@ $password = Configuration::getInstance()->getProperty( 'password', 'DATABASE' );
 $link = @mysql_connect( $host, $user, $password );
 if (! $link) {
 	echo_row( 'checking Property "DATABASE:host", "DATABASE:user", "DATABASE:password"', 'not OK', false );
-	$error ++;
+	$errors ++;
 } else {
 	echo_row( 'checking Property "DATABASE:host", "DATABASE:user", "DATABASE:password"', 'OK', true );
 }
@@ -170,47 +199,49 @@ $dbname = Configuration::getInstance()->getProperty( 'dbname', 'DATABASE' );
 $db_selected = @mysql_select_db( $dbname, $link );
 if (! $db_selected) {
 	echo_row( 'checking Property "DATABASE:dbname"', 'not OK', false );
-	$error ++;
+	$errors ++;
 } else {
 	echo_row( 'checking Property "DATABASE:dbname"', 'OK', true );
 }
 
 $cache = Configuration::getInstance()->getProperty( 'driver', 'CACHE' );
-if ($cache != 'REDIS' && $cache != 'MEMCACHED' && $cache != 'YAC' && $cache != 'NOOP') {
+if (($cache != 'REDIS' && $cache != 'MEMCACHED' && $cache != 'YAC' && $cache != 'NOOP') || ($cache == 'REDIS' && $redis_extension == false) || ($cache == 'MEMCACHED' && $memcached_extension == false) || ($cache == 'YAC' && $yac_extension == false)) {
 	echo_row( 'checking Property "CACHE:driver"', 'not OK', false );
-	$error ++;
+	$errors ++;
 } else {
-	echo_row( 'checking Property "CACHE:driver"', 'OK', true );
-}
 
-$cacheServer = Configuration::getInstance()->getProperty( 'server', 'CACHE' );
-$cachePort = ( int ) Configuration::getInstance()->getProperty( 'port', 'CACHE' );
-$result = null;
-switch ($cache) {
-	case 'REDIS' :
-		$cacheDelegate = new \Redis();
-		$result = $cacheDelegate->connect( $cacheServer, $cachePort );
-		break;
-	case 'MEMCACHED' :
-		$cacheDelegate = new \Memcached();
-		$result = $cacheDelegate->addServer( $cacheServer, $cachePort );
-		$version = $cacheDelegate->getStats();
-		$pid = array_values( $version )[0]['pid'];
-		$result = $pid > 0 ? true : false;
-		break;
-}
-if ($result !== null) {
-	if (! $result) {
-		echo_row( 'checking Property "CACHE:server", "CACHE:port"', 'not OK', false );
-		$error ++;
-	} else {
-		echo_row( 'checking Property "CACHE:server", "CACHE:port"', 'OK', true );
+	echo_row( 'checking Property "CACHE:driver"', 'OK', true );
+
+	$cacheServer = Configuration::getInstance()->getProperty( 'server', 'CACHE' );
+	$cachePort = ( int ) Configuration::getInstance()->getProperty( 'port', 'CACHE' );
+	$result = null;
+	switch ($cache) {
+		case 'REDIS' :
+			$cacheDelegate = new \Redis();
+			$result = $cacheDelegate->connect( $cacheServer, $cachePort );
+			break;
+		case 'MEMCACHED' :
+			$cacheDelegate = new \Memcached();
+			$result = $cacheDelegate->addServer( $cacheServer, $cachePort );
+			$version = $cacheDelegate->getStats();
+			$pid = array_values( $version )[0]['pid'];
+			$result = $pid > 0 ? true : false;
+			break;
+	}
+
+	if ($result !== null) {
+		if (! $result) {
+			echo_row( 'checking Property "CACHE:server", "CACHE:port"', 'not OK', false );
+			$errors ++;
+		} else {
+			echo_row( 'checking Property "CACHE:server", "CACHE:port"', 'OK', true );
+		}
 	}
 }
 $serializer = Configuration::getInstance()->getProperty( 'driver', 'SERIALIZER' );
 if ($serializer != 'IGBINARY' && $serializer != 'PHP') {
 	echo_row( 'checking Property "SERIALIZER:driver"', 'not OK', false );
-	$error ++;
+	$errors ++;
 } else {
 	echo_row( 'checking Property "SERIALIZER:driver"', 'OK', true );
 }
@@ -238,15 +269,91 @@ if ($link) {
 	if ($name === 'root')
 		$nameFound = true;
 }
-if (!$nameFound) {
+if (! $nameFound) {
 	echo_row( 'checking MySQL Database', 'not OK', false );
-	$error ++;
+	$errors ++;
 } else {
 	echo_row( 'checking MySQL Database', 'OK', true );
 }
+print_result( $errors, 0 );
 
-// check that property files can not be accessed! (.htaccess in place)
-// implement a heartbeat w/o auth in the rest server to check Slim
+/*
+ * *************************************************************************************
+ */
+
+print_headline( 'Checking Server .htaccess' );
+$errors = 0;
+
+$url = $serverurl . 'heartbeat';
+$http_response_header = array ();
+$result = @file_get_contents( $url, false, $context );
+if ($result == 'OK ') {
+	echo_row( 'checking RewriteRule in .htaccess', 'OK', true );
+} else {
+	echo_row( 'checking RewriteRule in .htaccess', 'not OK', false );
+	$errors ++;
+}
+$url = $serverurl . 'server.properties';
+$http_response_header = array ();
+$result = @file_get_contents( $url, false, $context );
+if (strpos( $http_response_header [0], '403' ) > 0) {
+	echo_row( 'checking that WWW access to "server.properties" is forbidden', 'OK', true );
+} else {
+	echo_row( 'checking that WWW access to "server.properties" is forbidden', 'not OK', false );
+	$errors ++;
+}
+print_result( $errors, 0 );
+
+/*
+ * *************************************************************************************
+ */
+
+print_headline( 'Checking Client .htaccess' );
+$errors = 0;
+
+$root = str_replace( 'check.php', '', getenv( 'SCRIPT_NAME' ) );
+$url = 'http://' . getenv( 'HTTP_HOST' ) . $root . 'client/client.properties';
+$http_response_header = array ();
+$result = @file_get_contents( $url, false, $context );
+if (strpos( $http_response_header [0], '403' ) > 0) {
+	echo_row( 'checking that WWW access to "client.properties" is forbidden', 'OK', true );
+} else {
+	echo_row( 'checking that WWW access to "client.properties" is forbidden', 'not OK', false );
+	$errors ++;
+}
+print_result( $errors, 0 );
+
+/*
+ * *************************************************************************************
+ */
+
+print_headline( 'Checking Client directory permissions' );
+$errors = 0;
+
+$directories = array (
+		'client/cache' => false,
+		'client/templates_c' => false,
+		'client/locale' => null,
+		'client/locale/1.conf' => null,
+		'client/locale/2.conf' => null,
+		'client/locale/languages.conf' => null
+);
+$root = dirname( __FILE__ ) . DIRECTORY_SEPARATOR;
+foreach ( $directories as $directory => $errorlevel ) {
+	if (is_writable( $root . $directory )) {
+		echo_row( 'check if file or directory "' . $directory . '" can be accessed for writing', 'OK', true );
+	} else {
+		if ($errorlevel === false) {
+			$msg = 'not OK';
+			$errors ++;
+		} else {
+			$msg = 'not OK<br>(for translations)';
+		}
+		echo_row( 'check if file or directory "' . $directory . '" can be accessed for writing', $msg, $errorlevel );
+	}
+}
+print_result( $errors, 0 );
+
 ?>
 </table>
 </body>
