5ada8d886feea75a083753eb4da500ba14d1251a
Oliver Lehmann <lehmann@ans-netz.de>
2019-03-24 00:35:01 +0100

    (c)

--- client/module/moduleImportedMoneyFlows.php
+++ client/module/moduleImportedMoneyFlows.php
@@ -1,7 +1,7 @@
 <?php
 
 //
-// Copyright (c) 2014-2015 Oliver Lehmann <oliver@laladev.org>
+// Copyright (c) 2014-2019 Oliver Lehmann <oliver@laladev.org>
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
